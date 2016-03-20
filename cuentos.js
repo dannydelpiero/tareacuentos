cf81a596532e91a4617ff57ccb852a5eea5622e2
Danny Del Pezo <ninedelpezo@gmail.com>
2016-03-20 18:03:04 -0500

    Pagina de Cuentos

---
+++ cuentos.js
@@ -0,0 +1,12 @@
+console.log("inicio de programa");
+var sustantivo = prompt("Ingresa un sustantivo: ");
+var adjetivo = prompt("Ingresa adjetivo: ");
+var verbo = prompt("Ingresa un verbo: ");
+document.write("<h1>El cuenta cuentos dice: </h1>");
+
+/*Los pequeños niños huyen del perro*/
+document.write("<p>Los "+ adjetivo + "  " + sustantivo + "  "+ verbo +" del perro.</p>");
+document.write("<p>Los "+ sustantivo + " " + adjetivo + " " + verbo +" del perro</p>");
+document.write("<p> " + verbo + " del perro los " +adjetivo+ " " +sustantivo + "</p>");
+
+console.log("Fin de programa");
